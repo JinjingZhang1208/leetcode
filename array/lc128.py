8993d8bce6e4e5a3eda1d6a9348616a5610fa9c4
JinjingZhang1208 <zhang.jinjin@northeastern.edu>
2023-11-02 00:19:34 -0700

    Create lc128.py

---
+++ array/lc128.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        # if not nums:
+        #     return 0  
+    
+        # sorted_nums = sorted(set(nums))
+
+        # resLength = 1
+        # data = 1
+        # for i in range(len(sorted_nums)):
+        #     if sorted_nums[i] - sorted_nums[i - 1] == 1:
+        #         resLength += 1
+        #     else:
+        #         data = max(resLength, data)
+        #         resLength = 1
+        # return max(resLength, data)
+        #  this method time complexity is O（nlogn）
+        # it ask O(n)
+        
