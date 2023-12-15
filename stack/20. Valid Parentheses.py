730b9a06b32252c9409bdb32e61d0f5b306a55cc
Jinjing Zhang <zhang.jinjin@northeastern.edu>
2023-12-15 00:52:04 -0800

    Create 20. Valid Parentheses.py

---
+++ stack/20. Valid Parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dic = {'}': '{', ']': '[', ')': '('}
+        stack = []
+
+        for i in s:
+            if i in dic:
+                if stack and stack[-1] == dic[i]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(i)
+
+        return not stack
+        # return len(stack) == 0
